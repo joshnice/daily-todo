7b9e0191e3bc732e9f4d34e9c37b4bb9d84721f5
Josh Nice <joshliamnice@gmail.com>
2022-09-21 11:31:40 +0400

    Added logic for page selection

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -1,21 +1,25 @@
 import { FunctionComponent } from "react";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import HeaderComponent from "../components/header";
 import HomePage from "../pages/home";
+import { Page } from "../pages/pages.types";
+import { pageAtom } from "../state/navigation";
 
-const router: FunctionComponent = () => {
-  return (
-    <>
-      <HeaderComponent />
-      <PageContainer>
-        <HomePage />
-      </PageContainer>
-    </>
-  );
+const Router: FunctionComponent = () => {
+    const selectedPage = useRecoilValue<Page>(pageAtom);
+    return (
+        <>
+        <HeaderComponent />
+        <PageContainer>
+            { Page.Home === selectedPage && <HomePage /> }
+        </PageContainer>
+        </>
+    );
 };
 
 const PageContainer = styled.div`
-  padding: 10px;
+    padding: 10px;
 `;
 
-export default router;
+export default Router;
